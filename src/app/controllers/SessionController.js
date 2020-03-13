984b47b8b557bc18f1dd21b32edeb2274fd06bc0
Orlando Gomes <orlandosjm@yahoo.com.br>
2020-03-12 22:17:31 -0300

    Final do módulo 2 gobarber

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,17 +1,30 @@
 import jwt from 'jsonwebtoken';
 
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 import auth from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .required()
+        .email(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
 
     if (!user) {
-      return res.staus(401).json({ error: 'Email does not exist' });
+      return res.status(401).json({ error: 'Email does not exist' });
     }
 
     if (!user.checkPassword(password)) {
